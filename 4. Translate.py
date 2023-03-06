56b1f99549b66b7b27df780bf4d81e7cbcaa62e6
Pablo-IT <pawelgorski.ekonomia@gmail.com>
2023-03-06 20:34:18 +0100

    Translating messages from PL into ENG.

--- 4. Translate.py
+++ 4. Translate.py
@@ -1,9 +1,26 @@
 import pandas as pd
 import numpy as np
 import pprint
+import googletrans
+from googletrans import Translator
 
 pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
-messages = pd.read_csv('C://Users//pawel//Datasets//Bike Trip//Final//Messages.csv', names = ['Date', 'Message_PL'], delimiter=',')
+translator = Translator()
+
+messages = pd.read_csv('C://Users//pawel//Datasets//Bike Trip//Final//Messages.csv', index_col='Date', delimiter=',', encoding='utf-8')
 messages = pd.DataFrame(messages)
-g
+
+messages.rename(columns={'Message':'Message_PL'}, inplace=True)
+messages.dropna(inplace=True)
+messages['Message_EN'] = ''
+
+translations = {}
+
+for msg in messages:
+    unique_elements = messages['Message_PL'].unique()
+    for element in unique_elements:
+        translations[element] = translator.translate(element, dest='english').text
+
+messages['Message_EN'] = messages['Message_PL'].replace(translations)
